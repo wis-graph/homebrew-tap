2fbfd34db47b552755b4c02f50be6a2d404f9fd3
wis <wis@wisui-Macmini.local>
2026-09-23 23:41:17 +0900

    chore: desklog 0.5.0

--- Formula/desklog.rb
+++ Formula/desklog.rb
@@ -1,9 +1,9 @@
 class Desklog < Formula
   desc "Records what you do at your desk as time spans in a local sqlite file"
   homepage "https://github.com/wis-graph/desklog"
-  url "https://github.com/wis-graph/desklog/releases/download/v0.4.2/desklog-0.4.2-macos-universal.tar.gz"
-  sha256 "3c08713fa1342596446d76916b1e07dba8b76d6f316787b70da3a7dc1854e7fc"
-  version "0.4.2"
+  url "https://github.com/wis-graph/desklog/releases/download/v0.5.0/desklog-0.5.0-macos-universal.tar.gz"
+  sha256 "43376b26bc63e4195dcdb8c3036afc9d0a997af872ca30d3cd5ce78c98c5189a"
+  version "0.5.0"
   license "MIT"
 
   # 미리 빌드해 Developer ID 로 서명·공증한 universal 바이너리를 받는다.
